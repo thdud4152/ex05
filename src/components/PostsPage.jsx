fb819b62f747de29df68d38478b8ecd83027f0f9
thdud4152 <thdud41522@gmail.com>
2023-04-01 11:30:41 +0900

    ver03

--- src/components/PostsPage.jsx
+++ src/components/PostsPage.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect, useState } from 'react'
-import { Row, Col, Table } from 'react-bootstrap'
+import { Row, Col, Table, Button } from 'react-bootstrap'
 
 const PostPage = () => {
-  const [list, setList]=useState([]);
+  const [list, setList] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [page, setPage] = useState(1);
+  const [last, setLast] = useState(1);
 
   const getPosts = () => {
     setLoading(true);
@@ -11,39 +13,51 @@ const PostPage = () => {
       .then(response => response.json())
       .then(json => {
         console.log(json);
-        setList(json);
+        let start = (page - 1) * 10 + 1;
+        let end = (page * 10);
+        setList(json.filter(post => post.id >= start && post.id <= end));
+        setLast(Math.ceil(json.length/5));
         setLoading(false);
       })
   }
 
   useEffect(() => {
     getPosts();
-  }, []);
+  }, [page]);
 
-  if(loading) return <h1 className='text-center my-5'>로딩중입니다....</h1>
+  if (loading) return <h1 className='text-center my-5'>로딩중입니다....</h1>
 
   return (
     <Row className='justfy-content-center'>
       <Col md={10}>
         <h1 className='text-center my-5'>게시글</h1>
         <Table striped bordered hover>
-            <thead>
-              <tr className='text-center'>
-                <td>ID.</td>
-                <td>Title</td>
-                
+          <thead>
+            <tr className='text-center'>
+              <td>ID.</td>
+              <td>Title</td>
+
+            </tr>
+          </thead>
+          <tbody>
+            {list.map(post =>
+              <tr key={post.id}>
+                <td>{post.id}</td>
+                <td>
+                  <div className='ellipsis'>{post.title}</div></td>
               </tr>
-            </thead>
-            <tbody>
-              {list.map(post=>
-                  <tr key={post.id}>
-                    <td>{post.id}</td>
-                    <td>
-                      <div className='ellipsis'>{post.title}</div></td>
-                  </tr>
-                )}
-            </tbody>
+            )}
+          </tbody>
         </Table>
+        <div className='text-center my-3'>
+          <Button
+            disabled={page === 1 && true}
+            onClick={() => setPage(page - 1)}>이전</Button>
+          <span className='px-3'>{page} / {last}</span>
+          <Button 
+          disabled={page===last && true}
+          onClick={() => setPage(page + 1)}>다음</Button>
+        </div>
       </Col>
     </Row>
   )
